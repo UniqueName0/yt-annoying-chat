a86db37ca0761a142f54d2621cff872fb5abf964
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2022-02-17 15:12:55 -0500

    eeee

--- README.md
+++ README.md
@@ -5,17 +5,25 @@ commands
 !stop - only usable by mods, stops the program.
 
 !move mouse - moves the mouse down and to the right
+or !mm
 
 !move left/right/down - moves in the selected direction for 1 second
+or !ml, !mr, !md
 
 !spam skills - spams all 3 skills
+or !ss
 
 !spam middle skill/top skill/bottom skill - spams the selected skill
+or !sm, !st, !sb
 
 !spam jump/attack - spams the selected option
+or !sj and !sa
 
 !spam stats - spam opens my stats menu
+or !sst
 
 !open shop - opens the shop if I'm in front of it
+or !os
 
 !close shop - closes the shop if I'm in it
+or !cs
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from youtubechat import YoutubeLiveChat, get_live_chat_id_for_stream_now
 import keyboard
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Key, Controller as Con
+
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
 import pyscreenshot as ImageGrab
-
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
@@ -19,74 +19,76 @@ chat_obj = YoutubeLiveChat("oauth_creds", [livechat_id])
 mouse = Controller()
 keyboard = Con()
 
+
 def respond(msgs, chatid):
     for msg in msgs:
         if str(msg) == "!stop" and msg.authorDetails.isChatModerator == True:
             chat_obj.send_message("stopping", chatid)
             exit()
-        if str(msg) == "!move mouse":
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-            mouse.move(10, 10)
-        if str(msg) == "!spam jump":
+        if str(msg) == "!move mouse" or str(msg) == "!mm":
+            mouse.move(100, 100)
+        if str(msg) == "!spam jump" or str(msg) == "!sj":
             keyboard.type("w")
-            time.sleep(0.1)
+            time.sleep(0.2)
             keyboard.type("w")
-            time.sleep(0.1)
+            time.sleep(0.2)
             keyboard.type("w")
-            time.sleep(0.1)
+            time.sleep(0.2)
             keyboard.type("w")
-            time.sleep(0.1)
+            time.sleep(0.2)
             keyboard.type("w")
-            time.sleep(0.1)
+            time.sleep(0.2)
             keyboard.type("w")
-        if str(msg) == "!move left":
+            time.sleep(0.2)
+            keyboard.type("w")
+            time.sleep(0.2)
+            keyboard.type("w")
+            time.sleep(0.2)
+            keyboard.type("w")
+            time.sleep(0.2)
+            keyboard.type("w")
+        if str(msg) == "!move left" or str(msg) == "!ml":
             keyboard.press("a")
-            time.sleep(1)
+            time.sleep(5)
             keyboard.release("a")
-        if str(msg) == "!move right":
+        if str(msg) == "!move right" or str(msg) == "!mr":
             keyboard.press("d")
-            time.sleep(1)
+            time.sleep(5)
             keyboard.release("d")
-        if str(msg) == "!move down":
+        if str(msg) == "!move down" or str(msg) == "!md":
             keyboard.press("s")
-            time.sleep(1)
+            time.sleep(5)
             keyboard.release("s")
-        if str(msg) == "!spam skills":
+        if str(msg) == "!spam skills" or str(msg) == "!ss":
             keyboard.type("./'")
-        if str(msg) == "!spam top skill":
             keyboard.type("'")
-        if str(msg) == "!spam middle skill":
             keyboard.type(".")
-        if str(msg) == "!spam bottom skill":
+        if str(msg) == "!spam top skill" or str(msg) == "!st":
+            keyboard.type("'")
+        if str(msg) == "!spam middle skill" or str(msg) == "!sm":
+            keyboard.type(".")
+        if str(msg) == "!spam bottom skill" or str(msg) == "!sb":
             keyboard.type("/")
-        if str(msg) == "!spam attack":
+        if str(msg) == "!spam attack" or str(msg) == "!sa":
             keyboard.press(Key.shift)
-            time.sleep(2)
+            time.sleep(5)
             keyboard.release(Key.shift)
-        if str(msg) == "!spam stats":
-            keyboard.type("*")
-            time.sleep(0.1)
-            keyboard.type("*")
-            time.sleep(0.1)
-            keyboard.type("*")
-            time.sleep(0.1)
-            keyboard.type("*")
-            time.sleep(0.1)
-            keyboard.type("*")
-            time.sleep(0.1)
-            keyboard.type("*")
-        if str(msg) == "!close shop":
+        if str(msg) == "!spam stats" or str(msg) == "!sst":
+            keyboard.type("=")
+            time.sleep(0.2)
+            keyboard.type("=")
+            time.sleep(0.2)
+            keyboard.type("=")
+            time.sleep(0.2)
+            keyboard.type("=")
+            time.sleep(0.2)
+            keyboard.type("=")
+            time.sleep(0.2)
+            keyboard.type("=")
+        if str(msg) == "!close shop" or str(msg) == "!cs":
             mouse.click(Button.right)
-        if str(msg) == "!open shop":
-            keyboard.type("////////")
+        if str(msg) == "!open shop" or str(msg) == "!os":
+            keyboard.type("/")
 
 
 try:
